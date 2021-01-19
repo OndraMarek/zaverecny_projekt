4479f90840fd94aa79bcdd01bae21fd708db5dab
OndraMarek <czondrejmarek@gmail.com>
2021-01-19 17:15:38 +0100

    1.0.0

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,19 @@
 #include <ESP8266WiFi.h>
 #include <ESPAsyncTCP.h>
 #include <ESPAsyncWebServer.h>
-
 #include <NTPClient.h>
 #include "WiFiUdp.h"
 #include <Servo.h>
-
 #include <ESPAsyncWiFiManager.h>
+#define DEBUG
 
 byte inputHodiny;
 byte inputMinuty;
 String inputCas;
 int pom = 0;
+unsigned long previousMillis = 0;
+unsigned long interval = 1000;
+int i=0;
 
 WiFiUDP ntpUDP;
 NTPClient timeClient(ntpUDP, "europe.pool.ntp.org");
@@ -19,53 +21,71 @@ NTPClient timeClient(ntpUDP, "europe.pool.ntp.org");
 const uint8_t servoPin = D2;
 Servo servo;
 
-int otevrit = 180;
-int zavrit = 0;
-
-// Set LED GPIO
-const int ledPin = 2;
-// Stores LED state
-String ledState;
-
 AsyncWebServer server(80);
 DNSServer dns;
 
+//Funkce pro spuštění krmení, vyvoláve se ve funkci feed()
+void davka(){
+  for(;i<8;){
+      unsigned long currentMillis = millis();
+      if(currentMillis - previousMillis > interval) {
+          previousMillis = currentMillis;
+          if (servo.read() == 180){
+              servo.write(0);
+              i++;
+          }
+          else{
+              servo.write(180);
+              i++;
+          }
+      }
+      yield();
+  }
+}
+
+//Ukládání času získaného z NTP serveru do proměnné time
 String getTime(){
-  String time = String(timeClient.getHours())+":"+String(timeClient.getMinutes());
-  Serial.println(time);
+  String getHodiny;
+  String getMinuty;
+  if(timeClient.getHours()>9){
+    getHodiny = String(timeClient.getHours());
+  }
+  else{
+    getHodiny = "0"+String(timeClient.getHours());
+  }
+
+  if(timeClient.getMinutes()>9){
+    getMinuty = String(timeClient.getMinutes());
+  }
+  else{
+    getMinuty = "0"+String(timeClient.getMinutes());
+  }
+  String time = getHodiny+":"+getMinuty;
   return String(time);
 }
 
+//Funkce porovná čas získaný z webové aplikace a čas z NTP serveru, v případě že se časy rovnají spouští funkci pro krmení
 void feed(){
   if (getTime() == inputCas && pom == 0){
     Serial.println("Feeding");
-    servo.write(otevrit);
-    delay(1000);
-    servo.write(zavrit);
+    davka();
     pom = 1;
+    i=0;
   }
   if (getTime() != inputCas){
     pom = 0;
   }
 }
 
+//Pokud funkce najde na html stránce symbol %TIME%, nahradí jej časem z funkce getTime()
 String processor(const String &var){
   Serial.println(var);
-  if (var == "STATE"){
-    if (digitalRead(ledPin)){
-      ledState = "ON";
-    }
-    else{
-      ledState = "OFF";
-    }
-    Serial.print(ledState);
-    return ledState;
-  }
-  else if (var == "TIME"){
+  if (var == "TIME"){
     return getTime();
   }
 }
 
+//Inicializace SPIFFS
 void initSpiffs(){
   if (!SPIFFS.begin()){
     Serial.println("An Error has occurred while mounting SPIFFS");
@@ -73,6 +93,7 @@ void initSpiffs(){
   }
 }
 
+//Funkce pro automatické připojení k WiFi síti pomoci knihovny ESPAsyncWiFiManager.h
 void wificonnect(){
   AsyncWiFiManager wifiManager(&server, &dns);
   wifiManager.autoConnect("AutoConnectAP");
@@ -80,6 +101,13 @@ void wificonnect(){
   Serial.println(WiFi.localIP());
 }
 
+//Inicializace NTP clientu a nastavení správného časového pásma 
+void timeClientFunc(){
+  timeClient.begin();
+  timeClient.setTimeOffset(3600);
+}
+
+//HTTP GET a POST requesty
 void htmlRequests(){
   server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
     request->send(SPIFFS, "/index.html", String(), false, processor);
@@ -93,16 +121,6 @@ void htmlRequests(){
     request->send(SPIFFS, "/main.js", String());
   });
 
-  server.on("/on", HTTP_GET, [](AsyncWebServerRequest *request) {
-    digitalWrite(ledPin, HIGH);
-    request->send(SPIFFS, "/index.html", String(), false, processor);
-  });
-
-  server.on("/off", HTTP_GET, [](AsyncWebServerRequest *request) {
-    digitalWrite(ledPin, LOW);
-    request->send(SPIFFS, "/index.html", String(), false, processor);
-  });
-
   server.on("/time", HTTP_GET, [](AsyncWebServerRequest *request) {
     request->send_P(200, "text/plain", getTime().c_str());
   });
@@ -114,16 +132,14 @@ void htmlRequests(){
 }
 
 void setup(){
-  Serial.begin(115200);
-  pinMode(ledPin, OUTPUT);
+  Serial.begin(115200); 
   servo.attach(servoPin);
 
   initSpiffs();
 
   wificonnect();
 
-  timeClient.begin();
-  timeClient.setTimeOffset(3600);
+  timeClientFunc();
 
   htmlRequests();
 
@@ -132,8 +148,5 @@ void setup(){
 
 void loop(){
   timeClient.update();
-  Serial.println(inputCas);
   feed();
-
-  delay(1000);
 }
